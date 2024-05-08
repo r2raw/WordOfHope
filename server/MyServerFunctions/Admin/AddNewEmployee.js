20fdeed864003016561e5d562c1c0be4bd201daf
r2raw <martejrii.arturo.07172001@gmail.com>
2024-05-08 15:36:04 +0800

    server

--- server/MyServerFunctions/Admin/AddNewEmployee.js
+++ server/MyServerFunctions/Admin/AddNewEmployee.js
@@ -2,9 +2,14 @@
 export default async (db, username,password, data, user, image)=>{
     try {
     const {firstname, lastname, middlename, suffix, sex, birthdate, email, phone, province, city, barangay, street, zip, position, department, empType, rfid} = data;
+
+    const selectPositionName = "SELECT position_name from positions where id=$1"
+
+    const positionName = await db.query(selectPositionName,[position])
+
     const sql = "INSERT INTO wohUser(username, password, userType, email, accountStatus, firstTimeLog) VALUES ($1,$2,$3,$4,$5,$6)"
     const sql1 = "INSERT INTO employee(id, firstname, lastname, middlename, suffix, sex, birthdate, phone, province, city, barangay, street, zip, position, department, empType, rfid, userId, createdBy, empImg) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11, $12,$13,$14,$15,$16,$17,$18,$19,$20)"
-    const insertUser = await db.query(sql, [username, password, position, email, "Activated", true]);
+    const insertUser = await db.query(sql, [username, password, positionName.rows[0].position_name, email, "Activated", true]);
 
     if(insertUser.rowCount > 0){
         const userIdResult = await db.query(
