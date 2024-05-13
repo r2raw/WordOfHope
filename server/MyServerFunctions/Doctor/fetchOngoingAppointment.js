ddd97923c573e898c18e036dae5f1d68cd7342bd
r2raw <martejrii.arturo.07172001@gmail.com>
2024-05-13 12:11:27 +0800

    asdasdasd

--- server/MyServerFunctions/Doctor/fetchOngoingAppointment.js
+++ server/MyServerFunctions/Doctor/fetchOngoingAppointment.js
@@ -1,6 +1,6 @@
 export default async (db, id)=>{
     try {
-        const sql = "SELECT q.*, aps.service, s.service_name, s.department_id, d.department_name  FROM queue AS q JOIN appointmentsched AS aps ON q.appointment_id = aps.id JOIN services AS s on aps.service = s.id JOIN departments AS d ON s.department_id = d.id WHERE aps.status='Ongoing' AND q.doctors_id=$1"
+        const sql = "SELECT q.*, aps.service, aps.method, aps.appointedfor, s.service_name, s.department_id, d.department_name  FROM queue AS q JOIN appointmentsched AS aps ON q.appointment_id = aps.id JOIN services AS s on aps.service = s.id JOIN departments AS d ON s.department_id = d.id WHERE aps.status='Ongoing' AND q.doctors_id=$1"
         const result = await db.query(sql, [id])
 
         return result.rows
--- server/MyServerFunctions/Nurse/getUserAppointment.js
+++ server/MyServerFunctions/Nurse/getUserAppointment.js
@@ -1,6 +1,6 @@
 export default async (db, appointmentId)=>{
     try {
-        const sql = "SELECT ua.*, up.firstname, up.lastname, up.middlename, up.suffix,aps.method, aps.appointmenttime, aps.appointmentdate, aps.service, s.service_type, s.service_name, s.department_id, d.department_name FROM userappointment AS ua JOIN userProfile AS up ON ua.userid = up.userid JOIN appointmentSched AS aps ON ua.id = aps.id JOIN services AS s ON aps.service = s.id JOIN departments AS d on s.department_id = d.id WHERE ua.id=$1"
+        const sql = "SELECT ua.*, up.firstname, up.lastname, up.middlename, up.suffix, up.email, up.phone, aps.method, aps.appointmenttime, aps.appointmentdate, aps.service, s.service_type, s.service_name, s.department_id, d.department_name, aps.appointedfor FROM userappointment AS ua JOIN userProfile AS up ON ua.userid = up.userid JOIN appointmentSched AS aps ON ua.id = aps.id JOIN services AS s ON aps.service = s.id JOIN departments AS d on s.department_id = d.id WHERE ua.id=$1"
         const result = await db.query(sql, [appointmentId]);
 
         return result.rows[0]
